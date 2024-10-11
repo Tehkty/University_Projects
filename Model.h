58b69d2369736138cac3876c87a14bb04c31b52c
Tehkty <sthefanyviveirosgoogle@hotmail.com>
2024-10-10 23:05:43 -0300

    Model.h

--- Model.h
+++ Model.h
@@ -1,12 +1,4 @@
-//DOM-GGMS-Model.h - Projeto Domino - Etapa 6
-//03/09/24 - Grupo: GGMS
-//Gabriel Azevedo Cruz 
-//Giulia Meninel Mattedi
-//Matheus Gomes Costa Castanho Giacomini
-//Sthefany Viveiros Cordeiro
-
-//estrutura tipo peca
-typedef struct {
+typedef struct {//estrutura tipo peca
     int lado1;
     int lado2;
     char status; //'J' = jogador 1, 'j' = jogador 2, 'M' = mesa, 'C' = compravel
@@ -21,4 +13,3 @@ typedef struct {
 
 void fCriar(tipopeca pecas[28]);
 void distribuiPecas(tipopeca pecas[28]);
-
