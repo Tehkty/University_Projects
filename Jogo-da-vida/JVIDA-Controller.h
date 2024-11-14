e6ab28ea0bd6d729a3d52a7df6d74c507bbdb8d5
Tehkty <sthefanyviveirosgoogle@hotmail.com>
2024-11-14 12:52:22 -0300

    JVIDA-Controller.h

--- Jogo-da-vida/JVIDA-Controller.h
+++ Jogo-da-vida/JVIDA-Controller.h
@@ -1,10 +1,5 @@
-//JVIDA-GGMS - Projeto Jogo da Vida - Etapa 4
-//Gabriel Azevedo Cruz
-//Giulia Meninel Mattedi
-//Matheus Gomes Costa Castanho Giacomini
-//Sthefany Viveiros Cordeiro
-#ifndef JVIDA_GGMS_Controller_h
-#define JVIDA_GGMS_Controller_h
+#ifndef JVIDA_Controller_h
+#define JVIDA_Controller_h
 ;
 void menu();
 void limparMapa();
